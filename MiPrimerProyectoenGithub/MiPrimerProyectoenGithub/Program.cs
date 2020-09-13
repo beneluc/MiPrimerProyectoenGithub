6e09e70aa78fd33c91db27aec992067f880d3426
beneluc <luciana.benelli@gmail.com>
2020-09-13 19:46:11 -0300

    Mi primer commit.

---
+++ MiPrimerProyectoenGithub/MiPrimerProyectoenGithub/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerProyectoenGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     ..-..");
+            Console.WriteLine("    ( o o )");
+            Console.WriteLine("    |  O  |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    |     |");
+            Console.WriteLine("    '~~~~~'");
+            Console.WriteLine("Hola a todos!!");
+        }
+    }
+}
